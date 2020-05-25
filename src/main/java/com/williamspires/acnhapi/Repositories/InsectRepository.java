868d995b4485189da7eefa1cd1204b9bff86fb86
Will <will@williamspires.co.uk>
2020-05-25 09:44:31 +0100

    Added ability to search by month and hemisphere

--- src/main/java/com/williamspires/acnhapi/Repositories/InsectRepository.java
+++ src/main/java/com/williamspires/acnhapi/Repositories/InsectRepository.java
@@ -48,6 +48,78 @@ public interface InsectRepository extends JpaRepository<Insect, String> {
     @Query(value = "select * from insects where NH_DEC <> 'NA' OR SH_DEC <> 'NA'", nativeQuery = true)
     List<Insect> insectsAvailableInDecember();
 
+    @Query(value = "select * from insects where NH_JAN <> 'NA'", nativeQuery = true)
+    List<Insect> insectsAvailableInJanuaryNH();
+
+    @Query(value = "select * from insects where SH_JAN <> 'NA'", nativeQuery = true)
+    List<Insect> insectsAvailableInJanuarySH();
+
+    @Query(value = "select * from insects where NH_FEB <> 'NA'", nativeQuery = true)
+    List<Insect> insectsAvailableInFebruaryNH();
+
+    @Query(value = "select * from insects where SH_FEB <> 'NA'", nativeQuery = true)
+    List<Insect> insectsAvailableInFebruarySH();
+
+    @Query(value = "select * from insects where NH_MAR <> 'NA'", nativeQuery = true)
+    List<Insect> insectsAvailableInMarchNH();
+
+    @Query(value = "select * from insects where SH_MAR <> 'NA'", nativeQuery = true)
+    List<Insect> insectsAvailableInMarchSH();
+
+    @Query(value = "select * from insects where NH_APR <> 'NA'", nativeQuery = true)
+    List<Insect> insectsAvailableInAprilNH();
+
+    @Query(value = "select * from insects where SH_APR <> 'NA'", nativeQuery = true)
+    List<Insect> insectsAvailableInAprilSH();
+
+    @Query(value = "select * from insects where NH_MAY <> 'NA'", nativeQuery = true)
+    List<Insect> insectsAvailableInMayNH();
+
+    @Query(value = "select * from insects where SH_MAY <> 'NA'", nativeQuery = true)
+    List<Insect> insectsAvailableInMaySH();
+
+    @Query(value = "select * from insects where NH_JUN <> 'NA'", nativeQuery = true)
+    List<Insect> insectsAvailableInJuneNH();
+
+    @Query(value = "select * from insects where SH_JUN <> 'NA'", nativeQuery = true)
+    List<Insect> insectsAvailableInJuneSH();
+
+    @Query(value = "select * from insects where NH_JUL <> 'NA'", nativeQuery = true)
+    List<Insect> insectsAvailableInJulyNH();
+
+    @Query(value = "select * from insects where SH_JUL <> 'NA'", nativeQuery = true)
+    List<Insect> insectsAvailableInJulySH();
+
+    @Query(value = "select * from insects where NH_AUG <> 'NA'", nativeQuery = true)
+    List<Insect> insectsAvailableInAugustNH();
+
+    @Query(value = "select * from insects where SH_AUG <> 'NA'", nativeQuery = true)
+    List<Insect> insectsAvailableInAugustSH();
+
+    @Query(value = "select * from insects where NH_SEP <> 'NA'", nativeQuery = true)
+    List<Insect> insectsAvailableInSeptemberNH();
+
+    @Query(value = "select * from insects where SH_SEP <> 'NA'", nativeQuery = true)
+    List<Insect> insectsAvailableInSeptemberSH();
+
+    @Query(value = "select * from insects where NH_OCT <> 'NA'", nativeQuery = true)
+    List<Insect> insectsAvailabeInOctoberNH();
+
+    @Query(value = "select* from insects where SH_OCT <> 'NA'", nativeQuery = true)
+    List<Insect> insectsAvailableInOctoberSH();
+
+    @Query(value = "select * from insects where NH_NOV <> 'NA'", nativeQuery = true)
+    List<Insect> insectsAvailableInNovemberNH();
+
+    @Query(value = "select * from insects where SH_NOV <> 'NA'", nativeQuery = true)
+    List<Insect> insectsAvailableInNovemberSH();
+
+    @Query(value = "select * from insects where NH_DEC <> 'NA'", nativeQuery = true)
+    List<Insect> insectsAvailableInDecemberNH();
+
+    @Query(value = "select * from insects where SH_DEC <> 'NA'", nativeQuery = true)
+    List<Insect> insectsAvailableInDecemberSH();
+
     @Query(value = "select * from insects", nativeQuery = true)
     List<Insect> getAllInsects();
 }
